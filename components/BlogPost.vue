28c3fbf172fc1bcf29d1c15c2a90bcbe7b2243e4
christian harrison <christianh@live.co.uk>
2023-04-24 14:28:29 +0100

    Creates reusable blogPost comp

--- components/BlogPost.vue
+++ components/BlogPost.vue
@@ -1,29 +1,28 @@
 <template>
-  <div class="mb-8">
-  <h1 class="text-4xl font-semibold">{{ title }}</h1>
-  <p class="italic mb-4">{{ about }}</p>
-  <iframe src:="link" class="w-full h-[500px] border-none"></iframe>
-  </div>
+  <a :href="link" target="_blank" class="mb-8">
+    <h1 class="text-4xl font-bold">{{ title }}</h1>
+    <p class="italic mb-4">{{ about }}</p>
+    <img :src="imageSrc" class="" />
+  </a>
 </template>
 
 <script setup lang="ts">
-
-const props = defineProps({
-  title: {
-    type: String,
-    required: true
-  },
-  about: {
-    type: String,
-    required: true
-  },
-  link: {
-    type: String,
-    required: true
-  },
-})
-
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    imageSrc: {
+      type: String,
+      required: true,
+    },
+  });
 </script>
-
-
-
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       title="test blog post"
       about="test about section that reads like this"
       link="https://github.com/christywastaken/twitter-sentiment-analysis"
+      image-src="/blog_post_images/twitter-sentiment-anlysis-image.png"
     />
   </div>
 </template>
