be76cbaa66ff3bd383ca3af027a5817b41b80c4d
christian harrison <christianh@live.co.uk>
2023-04-30 17:56:17 +0100

    Removes autocomplete from contact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,10 +1,10 @@
 <template>
   <form class="flex flex-col items-center justify-center px-4 py-4" @submit.prevent="submitForm">
-    <input class="my-4 w-full px-5 text-white bg-transparent border-b-2 h-10" type="text" id="name" v-model="form.name" placeholder="Your name" required />
+    <input class="my-4 w-full px-5 text-white bg-transparent border-b-2 h-10 focus:outline-none focus:bg-transparent" type="text" id="name" v-model="form.name" required placeholder="Your name" autocomplete="off" />
     
-    <input class="my-4 w-full px-5 text-white bg-transparent border-b-2 h-10" type="email" id="email" v-model="form.email" required placeholder="Your email" />
+    <input class="my-4 w-full px-5 text-white bg-transparent border-b-2 h-10 focus:outline-none focus:bg-transparent" type="email" id="email" v-model="form.email" required placeholder="Your email" autocomplete="off"/>
 
-    <textarea class="my-4 w-full px-5 text-white bg-transparent border-b-2 h-10" id="message" v-model="form.message" required placeholder="Your message for me"></textarea>
+    <textarea class="my-4 w-full px-5 text-white bg-transparent border-b-2 h-10 focus:outline-none focus:bg-transparent" id="message" v-model="form.message" required placeholder="Your message for me" autocomplete="off"></textarea>
 
     <button class="flex items-center justify-center rounded-full bg-gray-400 px-8" type="submit">Send</button>
   </form>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <template>
   <div class="flex flex-col min-h-screen bg-primary">
-    <div>top</div>
-    <div>bottom</div>
   </div>
 </template>
