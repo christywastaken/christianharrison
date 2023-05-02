9654a0cbe2836b1b59b66b6fe73a7602959d2017
christian harrison <christianh@live.co.uk>
2023-05-02 13:15:33 +0100

    Adds dynamic header funcionality

--- components/navigation/DesktopHeader.vue
+++ components/navigation/DesktopHeader.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="flex justify-between items-center px-[10%] h-12 bg-white">
-    <div class=" font-semibold">
-      <NuxtLink :to="{ name: 'index' }">Christian Harrison</NuxtLink>
-    </div>
-    <div class="flex items-center space-x-4">
-      <NuxtLink :to="{ name: 'index' }" class="hover:text-gray-500">Home</NuxtLink>
-      <NuxtLink :to="{ name: 'portfolio' }" class="hover:text-gray-500">Portfolio</NuxtLink>
-      <NuxtLink :to="{ name: 'contact' }" class="hover:text-gray-500">Contact</NuxtLink>
-    </div>
+  <div class="flex items-center space-x-4">
+    <NuxtLink :to="{ name: 'index' }" class="hover:text-gray-500">Home</NuxtLink>
+    <NuxtLink :to="{ name: 'portfolio' }" class="hover:text-gray-500">Portfolio</NuxtLink>
+    <NuxtLink :to="{ name: 'contact' }" class="hover:text-gray-500">Contact</NuxtLink>
   </div>
 </template>
--- components/navigation/Header.vue
+++ components/navigation/Header.vue
@@ -1,1 +1,9 @@
-
+<template>
+  <div class="flex justify-between items-center px-[10%] h-12 bg-white">
+    <div class="font-semibold">
+      <NuxtLink :to="{ name: 'index' }">Christian Harrison</NuxtLink>
+    </div>
+    <NavigationMobileHeader class="block md:hidden" />
+    <NavigationDesktopHeader class="hidden md:block" />
+  </div>
+</template>
