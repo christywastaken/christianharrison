d08f89f117ca7ac119c144d30b05d09a7e4fbdc5
christian harrison <christianh@live.co.uk>
2023-05-04 19:12:17 +0100

    Adds new blogpost component

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="flex flex-col items-center p-4">
+    <BlogPost 
+      title="This Website"
+      about="This website serves as both a learning experience and a portfolio to showcase my learning."
+      link="https://github.com/christywastaken/christianharrison"
+      image-src="/blog_post_images/christianharrison.png"
+      class="max-w-3xl"
+    />
     <BlogPost
       title="Tweet Sentiment Analysis: Exploring Temporal Patterns"
       about="test about section that reads like this"
       link="https://github.com/christywastaken/twitter-sentiment-analysis"
       image-src="/blog_post_images/tweet-sentiment-anlysis.png"
-      class="max-w-2xl"
+      class="max-w-3xl"
     />
     <BlogPost
       title="SendJoy Web App"
       about="test about section that reads like this"
       link="https://sendjoy.app"
       image-src="/blog_post_images/sendjoy.png"
-      class="max-w-2xl"
+      class="max-w-3xl"
     />
     <BlogPost
       title="Python API for SendJoy Backend"
       about="test about section that reads like this"
       link="https://github.com/christywastaken/"
       image-src="/blog_post_images/tweet-sentiment-anlysis.png"
-      class="max-w-2xl"
+      class="max-w-3xl"
     />
     <BlogPost
       title="iOS App for SendJoy Backend Utilities"
       about="test about section that reads like this"
       link="https://github.com/christywastaken/"
       image-src="/blog_post_images/ios-app-sendjoy.png"
-      class="max-w-2xl"
+      class="max-w-3xl"
     />
   </div>
 </template>
