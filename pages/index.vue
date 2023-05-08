6a0828584abd220529d667fdb06dda442c5d2644
christian harrison <christianh@live.co.uk>
2023-05-08 19:54:09 +0100

    Adds massive hg logo that needs fixing lol

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <div class="flex flex-row items-center justify-end mt-20 me-20">
-      <div class="text-white mx-20 italic">Driven by a lifelong fascination with technology, I began my journey into software engineering in January 2023. With a passion for using technology to make a positive impact, I am eager to continue learning and growing.</div>
+      <div class="text-white mx-20 italic">
+        Driven by a lifelong fascination with technology, I began my journey into software engineering in January 2023.
+        With a passion for using technology to make a positive impact, I am eager to continue learning and growing.
+      </div>
       <img src="/misc/profile-pic.png" class="h-64 w-64" />
     </div>
+    <a href="https://github.com/christywastaken" class="h-16 w-16">
+      <img src="/misc/GitHub_Logo_White.png" class="h-full w-full"/>
+    </a>
   </div>
 </template>
