60b9fecd1384757ea7769e1b47989df540e1ccee
christian harrison <christianh@live.co.uk>
2023-05-09 12:39:06 +0100

    Adds blogpostdekstop and blogpostmobile.vue

---
+++ components/BlogPostDesktop.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <a :href="link" target="_blank" class="flex flex-col justify-center p-4 my-8 rounded-md bg-white">
+    <h2 class="md:text-3xl text-xl font-bold">{{ title }}</h2>
+    <div class="flex flex-row justify-center pt-4">
+      <p class="w-1/2 italic mb-4">{{ about }}</p>
+      <img :src="imageSrc" class="w-1/2 object-contain rounded-md" />
+    </div>
+  </a>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    title?: String;
+    about?: String;
+    link?: String;
+    imageSrc?: String;
+  }>();
+</script>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="flex flex-col items-center p-4">
-    <BlogPost 
+    <BlogPostDesktop 
       title="This Website"
       about="This website serves as both a learning experience and a portfolio to showcase my learning."
       link="https://github.com/christywastaken/christianharrison"
       image-src="/blog_post_images/christianharrison.png"
       class="max-w-3xl"
     />
-    <BlogPost
+    <BlogPostDesktop
       title="Tweet Sentiment Analysis: Exploring Temporal Patterns"
       about="test about section that reads like this"
       link="https://github.com/christywastaken/twitter-sentiment-analysis"
       image-src="/blog_post_images/tweet-sentiment-anlysis.png"
       class="max-w-3xl"
     />
-    <BlogPost
+    <BlogPostDesktop
       title="SendJoy Web App"
       about="test about section that reads like this"
       link="https://sendjoy.app"
       image-src="/blog_post_images/sendjoy.png"
       class="max-w-3xl"
     />
-    <BlogPost
+    <BlogPostDesktop
       title="Python API for SendJoy Backend"
       about="test about section that reads like this"
       link="https://github.com/christywastaken/"
       image-src="/blog_post_images/tweet-sentiment-anlysis.png"
       class="max-w-3xl"
     />
-    <BlogPost
+    <BlogPostDesktop
       title="iOS App for SendJoy Backend Utilities"
       about="test about section that reads like this"
       link="https://github.com/christywastaken/"
