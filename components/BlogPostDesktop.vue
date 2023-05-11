100a282a048cd2df0d29a5098b453a5c20438464
christian harrison <christianh@live.co.uk>
2023-05-11 10:53:06 +0100

    Impoves scaling ui in portfolio

--- components/BlogPostDesktop.vue
+++ components/BlogPostDesktop.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <a :href="link" target="_blank" class="flex flex-col justify-center p-4 my-8 rounded-md bg-white">
+  <a :href="link" target="_blank" class="flex flex-col justify-center p-4 my-8 rounded-md bg-white max-w-4xl">
     <h2 class="md:text-3xl text-xl font-bold">{{ title }}</h2>
     <div class="flex flex-row justify-center pt-4">
       <p class="w-1/2 italic mb-4">{{ about }}</p>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="flex-col items-center p-4 px-[10%]">
+  <div class="flex flex-col items-center p-4 md:mx-[10%] mx-[5%]">
     <li v-for="(item, index) in blogPostItems" :key="index" class="mt-8 hidden md:flex">
       <BlogPostDesktop :title="item.title" :about="item.about" :image-src="item.image" :link="item.link"/>
     </li>
