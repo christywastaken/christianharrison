01cc90524b01cd33bb3ce12f21152e636356b179
christian harrison <christianh@live.co.uk>
2023-05-11 12:27:16 +0100

    Adds new blogpost

--- config/index.ts
+++ config/index.ts
@@ -9,8 +9,9 @@ export const menuItems: MenuItem[] = [
 ];
 
 export const blogPostItems: BlogPostItem[] = [
-  { title: "This Website", about:"This website serves as both a learning experience and a portfolio to showcase my learning.", link:"https://github.com/christywastaken/christianharrison", image: "/blog_post_images/christianharrison.png" },
+  { title:"This Website", about:"This website serves as both a learning experience and a portfolio to showcase my learning.", link:"https://github.com/christywastaken/christianharrison", image: "/blog_post_images/christianharrison.png" },
   { title:"Tweet Sentiment Analysis: Exploring Temporal Patterns", about:"test about section that reads like this", link:"https://github.com/christywastaken/twitter-sentiment-analysis", image:"/blog_post_images/tweet-sentiment-anlysis.png" },
+  { title:"Twitscrape - A Seleniumwire Twitter Scraping Package", about:"Test about section that reads like this.", link:"https://pypi.org/project/twitscrape/", image:"blog_post_images/twitscrape.png" },
   { title:"SendJoy Web App", about:"test about section that reads like this", link:"https://sendjoy.app", image:"/blog_post_images/sendjoy.png" },
   { title:"Python API for SendJoy Backend", about:"test about section that reads like this", link:"https://github.com/christywastaken/", image:"/blog_post_images/tweet-sentiment-anlysis.png"},
   { title:"iOS App for SendJoy Backend Utilities", about:"test about section that reads like this", link:"https://github.com/christywastaken/", image:"/blog_post_images/ios-app-sendjoy.png"},
