c0cbb2465b208481abadc7dd0f7cba949630f3d6
christian harrison <christianh@live.co.uk>
2023-05-15 11:55:29 +0100

    Styles and refactors index.vue for mobile and desktop screens

---
+++ components/MobileIndex.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-pink-500">
+    <div class="flex flex-col md:mt-64 mt-20 items-center justify-end mx-[10%]">
+      <div class="flex flex-row justify-center max-h-64">
+        <div class="flex flex-col w-1/2 items-center me-20">
+          <div class="text-white italic p-8 text-lg">
+            Driven by a lifelong fascination with technology, I began my journey into software engineering in January
+            2023. With a passion for using technology to make a positive impact, I am eager to continue learning and
+            growing.
+          </div>
+          <div class="flex flex-row">
+            <Icon name="python" class="h-12 w-12" />
+            <Icon name="swift" class="h-12 w-12" />
+            <Icon name="javascript" class="h-12 w-12" />
+            <Icon name="html" class="h-12 w-12" />
+            <Icon name="css" class="h-12 w-12" />
+            <Icon name="mongo" class="h-12 w-12" />
+            <Icon name="sql" class="h-12 w-12" />
+            <Icon name="nuxt" class="h-12 w-12" />
+            <Icon name="vue" class="h-12 w-12" />
+            <Icon name="numpy" class="h-12 w-12" />
+            <Icon name="pandas" class="h-12 w-12" />
+          </div>
+        </div>
+        <div class="w-64 h-64">
+          <img src="/misc/profile-pic.png" class="w-full h-full object-contain" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,6 @@
 <template>
   <div>
-    <div class="flex flex-col md:mt-40 mt-20 items-center justify-end mx-[10%]">
-      <div class="text-white italic p-8 text-sm md:text-lg text-center md:mx-[20%]">
-        Driven by a lifelong fascination with technology, I began my journey into software engineering in January 2023.
-        With a passion for using technology to make a positive impact, I am eager to continue learning and growing.
-        <br /><br />
-        This website will showcase my journey.
-      </div>
-      <div class="flex flex-row justify-between max-h-64">
-        <div class="flex w-1/2 justify-center">
-          <Icon name="python" class="h-12 w-12" />
-          <Icon name="swift" class="h-12 w-12" />
-          <Icon name="javascript" class="h-12 w-12" />
-          <Icon name="html" class="h-12 w-12" />
-          <Icon name="css" class="h-12 w-12" />
-          <Icon name="mongo" class="h-12 w-12" />
-          <Icon name="sql" class="h-12 w-12" />
-          <Icon name="nuxt" class="h-12 w-12" />
-          <Icon name="vue" class="h-12 w-12" />
-          <Icon name="numpy" class="h-12 w-12" />
-          <Icon name="pandas" class="h-12 w-12" />
-
-        </div>
-        <div class="w-1/2">
-          <img src="/misc/profile-pic.png" class="w-full h-full object-contain" />
-        </div>
-      </div>
-    </div>
+   <DesktopIndex class="hidden md:flex" />
+   <MobileIndex class="flex md:hidden" />
   </div>
 </template>
