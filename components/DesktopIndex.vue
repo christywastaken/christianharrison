f0df1f1b58b5ac9af0cc06e854ee0c7aeef2780e
christian harrison <christianh@live.co.uk>
2023-05-16 10:49:43 +0100

    Fixes svg index scaling issue and refactors code

--- components/DesktopIndex.vue
+++ components/DesktopIndex.vue
@@ -1,31 +1,25 @@
 <template>
-  <div>
-    <div class="flex flex-col md:mt-64 mt-20 items-center justify-end mx-[10%]">
-      <div class="flex flex-row justify-center max-h-64">
-        <div class="flex flex-col w-1/2 items-center me-20">
-          <div class="text-white italic p-8 text-lg">
-            Driven by a lifelong fascination with technology, I began my journey into software engineering in January
-            2023. With a passion for using technology to make a positive impact, I am eager to continue learning and
-            growing.
-          </div>
-          <div class="flex flex-row">
-            <Icon name="python" class="h-12 w-12" />
-            <Icon name="swift" class="h-12 w-12" />
-            <Icon name="javascript" class="h-12 w-12" />
-            <Icon name="html" class="h-12 w-12" />
-            <Icon name="css" class="h-12 w-12" />
-            <Icon name="mongo" class="h-12 w-12" />
-            <Icon name="sql" class="h-12 w-12" />
-            <Icon name="nuxt" class="h-12 w-12" />
-            <Icon name="vue" class="h-12 w-12" />
-            <Icon name="numpy" class="h-12 w-12" />
-            <Icon name="pandas" class="h-12 w-12" />
-          </div>
+  <div class="flex flex-col md:mt-64 mt-20 items-center justify-center mx-[10%]">
+    <div class="flex flex-row items-center justify-center max-h-64">
+      <div class="flex flex-col px-8 w-1/2 items-center mx-20">
+        <div class="text-white italic text-lg">
+          Driven by a lifelong fascination with technology, I began my journey into software engineering in January
+          2023. With a passion for using technology to make a positive impact, I am eager to continue learning and
+          growing.
         </div>
-        <div class="w-64 h-64">
-          <img src="/misc/profile-pic.png" class="w-full h-full object-contain" />
+        <div class="flex flex-row mt-20">
+          <img v-for="(item, index) in stackImagesArray" :key="index"  :src="`/stack-images/${item}.svg`" class="h-12 w-12" />
         </div>
+      </div>
+      <div class="w-64 h-64">
+        <img src="/misc/profile-pic.png" class="w-full h-full object-contain" />
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+
+  const stackImagesArray = ["python", "swift", "javascript", "html", "css", "mongo", "sql", "nuxt", "vue", "numpy", "pandas"]
+
+</script>
