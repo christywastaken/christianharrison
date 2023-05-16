75ced4d7c9c7467ff780c52f2f0b4e50657d9574
christian harrison <christianh@live.co.uk>
2023-05-16 10:56:05 +0100

    MobileIndex vue now copies DesktopIndex

--- components/DesktopIndex.vue
+++ components/DesktopIndex.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col md:mt-64 mt-20 items-center justify-center mx-[10%]">
+  <div class="flex flex-col md:mt-60 mt-20 items-center justify-center mx-[10%]">
     <div class="flex flex-row items-center justify-center max-h-64">
       <div class="flex flex-col px-8 w-1/2 items-center mx-20">
         <div class="text-white italic text-lg">
@@ -8,7 +8,12 @@
           growing.
         </div>
         <div class="flex flex-row mt-20">
-          <img v-for="(item, index) in stackImagesArray" :key="index"  :src="`/stack-images/${item}.svg`" class="h-12 w-12" />
+          <img
+            v-for="(item, index) in stackImagesArray"
+            :key="index"
+            :src="`/stack-images/${item}.svg`"
+            class="h-11 w-11"
+          />
         </div>
       </div>
       <div class="w-64 h-64">
@@ -19,7 +24,17 @@
 </template>
 
 <script setup>
-
-  const stackImagesArray = ["python", "swift", "javascript", "html", "css", "mongo", "sql", "nuxt", "vue", "numpy", "pandas"]
-
+  const stackImagesArray = [
+    "python",
+    "swift",
+    "javascript",
+    "html",
+    "css",
+    "mongo",
+    "sql",
+    "nuxt",
+    "vue",
+    "numpy",
+    "pandas",
+  ];
 </script>
