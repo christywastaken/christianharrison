31513b8bccef683dbc391e319467a9eacfa6cf26
christian harrison <christianh@live.co.uk>
2023-05-21 15:00:40 +0100

    Adds spinner and dynamic text on button while sending email

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="flex flex-col items-center justify-center px-4 py-4" @submit.prevent="submitForm">
+  <form v-if="!requestStatus.success && !requestStatus.error" class="flex flex-col items-center justify-center px-4 py-4" @submit.prevent="submitForm">
     <input
       class="my-4 w-full px-5 text-white bg-transparent border-b-2 h-10 focus:outline-none focus:bg-transparent"
       type="text"
@@ -29,21 +29,28 @@
       autocomplete="off"
     />
 
-    <div v-if="requestStatus.success" class="h-12 my-4 bg-green-500">Success</div>
-    <div v-if="requestStatus.error" class="h-12 my-4 bg-red-500">Failure: {{ requestStatus.errorMessage }}</div>
-
+    
     <button class="flex items-center justify-center rounded-md mt-8 px-8 bg-slate-100" type="submit">
-      Send <Icon name="paperplane" class="m-2 h-4 w-4 stroke-black"></Icon>
+      {{ buttonText }} <Icon v-if="!requestStatus.waiting" name="paperplane" class="m-2 h-4 w-4 stroke-black"></Icon> <Icon v-if="requestStatus.waiting" name="spinner" class="m-2 h-4 w-4 fill-black icon-spin"></Icon>
     </button>
   </form>
+  <div v-if="requestStatus.success" class="h-12 my-4 bg-green-500">Success</div>
+  <div v-if="requestStatus.error" class="h-12 my-4 bg-red-500">Failure: {{ requestStatus.errorMessage }}</div>
+  
+
 </template>
 
 <script setup lang="ts">
 
   const requestStatus = ref({
+    waiting: false,
     success: false,
     error: false,
     errorMessage: ""
+  })
+
+  const buttonText = computed(() => {
+    return requestStatus.value.waiting ? 'Sending' : 'Send'
   })
 
   const form = ref({
@@ -53,6 +60,7 @@
   });
 
   const submitForm = () => {
+    requestStatus.value.waiting = true
     $fetch("/api/send-email", {
       method: "POST",
       body: {
@@ -81,4 +89,17 @@
     appearance: none;
     border-radius: 0;
   }
+</style>
+
+<style scoped>
+
+  @keyframes spin { 
+    0% { transform: rotate(0deg); }
+    100% {transform: rotate(360deg); }
+   }
+
+   .icon-spin {
+    animation: spin 1s linear infinite;
+   }
+
 </style>
