fcb16369ac989eb668c75c50876d09080c92f5fc
christian harrison <christianh@live.co.uk>
2023-05-23 12:37:56 +0100

    Adds some text to blogposts

--- components/BlogPostDesktop.vue
+++ components/BlogPostDesktop.vue
@@ -5,7 +5,7 @@
     <a :href="link" target="_blank" class="flex flex-col justify-center p-4 my-4 rounded-md bg-white max-w-4xl hover:white-shadow">
       <h2 class="md:text-3xl text-xl font-bold">{{ title }}</h2>
       <div class="flex flex-row justify-center pt-4">
-        <p class="w-1/2 mb-4 text-sm pe-2" v-html="about"></p>
+        <p class="w-1/2 mb-4 text-sm pe-4" v-html="about"></p>
         <img :src="imageSrc" class="w-1/2 object-contain rounded-md" />
       </div>
     </a>
--- config/index.ts
+++ config/index.ts
@@ -9,7 +9,7 @@ export const menuItems: MenuItem[] = [
 ];
 
 export const blogPostItems: BlogPostItem[] = [
-  { title:"This Website", about:`- This website serves as both a learning experience and a portfolio to showcase my learning.<br/><br/>- Although web development`, link:"https://github.com/christywastaken/christianharrison", image: "/blog_post_images/christianharrison.png" },
+  { title:"This Website", about:`- This website serves as both a learning experience and a portfolio to showcase my learning.<br/><br/>- I'm trying to work with many different bits of tech. Building websites has been a great help in broadening my base knowldege.<br/><br/>- This website is built using the Nuxt3 framework and deployed on Vercel (wow, that was easy!)`, link:"https://github.com/christywastaken/christianharrison", image: "/blog_post_images/christianharrison.png" },
   { title:"Tweet Sentiment Analysis: Exploring Temporal Patterns", about:"test about section that reads like this", link:"https://github.com/christywastaken/twitter-sentiment-analysis", image:"/blog_post_images/tweet-sentiment-anlysis.png" },
   { title:"Twitscrape - A Seleniumwire Twitter Scraping Package", about:"Test about section that reads like this.", link:"https://pypi.org/project/twitscrape/", image:"blog_post_images/twitscrape.png" },
   { title:"SendJoy Web App", about:"test about section that reads like this", link:"https://sendjoy.app", image:"/blog_post_images/sendjoy.png" },
