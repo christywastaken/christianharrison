e4b1db9d6e748f882deb4e8165b3275d6932fdeb
christian harrison <christianh@live.co.uk>
2023-05-23 12:23:47 +0100

    updates blogposts to use v-html

--- components/BlogPostMobile.vue
+++ components/BlogPostMobile.vue
@@ -1,7 +1,7 @@
 <template>
   <a :href="link" target="_blank" class="flex flex-col justify-center p-4 my-8 rounded-md bg-white hover:white-shadow">
     <h2 class="md:text-3xl text-xl font-bold">{{ title }}</h2>
-    <p class="italic mb-4">{{ about }}</p>
+    <p class="italic mb-4" v-html="about"></p>
     <img :src="imageSrc" class="object-contain rounded-md" />
   </a>
 </template>
