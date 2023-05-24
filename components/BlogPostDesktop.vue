76573b9fd5c9c80caf956bcca2169644b2705123
christian harrison <christianh@live.co.uk>
2023-05-24 15:39:26 +0100

    Small UI change

--- components/BlogPostDesktop.vue
+++ components/BlogPostDesktop.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="">
     <a :href="link" target="_blank" class="absolute top-0 left-0 hover:white-shadow" />
-    <a :href="link" target="_blank" class="flex flex-col justify-center p-4 my-4 rounded-md bg-white max-w-4xl hover:white-shadow">
+    <a :href="link" target="_blank" class="flex flex-col justify-center p-4 my-4 rounded-md bg-slate-50 max-w-4xl hover:white-shadow">
       <h2 class="md:text-3xl text-xl font-bold">{{ title }}</h2>
       <div class="flex flex-row justify-center pt-4">
         <p class="w-1/2 mb-4 text-sm pe-4" v-html="about"></p>
