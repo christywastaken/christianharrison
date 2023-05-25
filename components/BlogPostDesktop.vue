4a829ff0831a409adbeb363440ca887e3ec9eb90
christian harrison <christianh@live.co.uk>
2023-05-25 10:54:46 +0100

    New copy added

--- components/BlogPostDesktop.vue
+++ components/BlogPostDesktop.vue
@@ -2,10 +2,10 @@
 <template>
   <div class="">
     <a :href="link" target="_blank" class="absolute top-0 left-0 hover:white-shadow" />
-    <a :href="link" target="_blank" class="flex flex-col justify-center p-4 my-4 rounded-md bg-slate-50 max-w-4xl hover:white-shadow">
-      <h2 class="md:text-3xl text-xl font-bold">{{ title }}</h2>
-      <div class="flex flex-row justify-center pt-4">
-        <p class="w-1/2 mb-4 text-sm pe-4" v-html="about"></p>
+    <a :href="link" target="_blank" class="flex flex-col justify-center p-7 my-4 rounded-md bg-slate-50 max-w-4xl hover:white-shadow">
+      <h2 class="md:text-3xl text-xl font-bold mb-4">{{ title }}</h2>
+      <div class="flex flex-row justify-center pt-4 space-x-8 items-center">
+        <p class="w-1/2 mb-4 text-sm" v-html="about"></p>
         <img :src="imageSrc" class=" w-1/2 h-auto rounded-md" />
       </div>
     </a>
